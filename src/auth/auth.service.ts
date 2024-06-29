559d489635ffc9f6b905c51823b20498d9391667
Renato Marques <megadeth.destroys.metallica@gmail.com>
2024-06-29 01:45:32 -0300

    feature/implemented jwt authentication and auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { Prisma, PrismaClient, User } from '@prisma/client'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private expiresIn: string
+
+  constructor(
+    private configService: ConfigService,
+    private jwt: JwtService,
+    private prisma: PrismaClient,
+  ) {
+    this.expiresIn = this.configService.get<string>('JWT_EXPIRES_IN')
+  }
+
+  async signUp(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({ data })
+  }
+}
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { User } from '@prisma/client'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    configService: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    })
+  }
+
+  async validate(payload: {
+    sub: number
+    email: string
+    tokenVersion: number
+  }): Promise<User> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    })
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    if (user.tokenVersion !== payload.tokenVersion) {
+      throw new UnauthorizedException('Token is invalid.')
+    }
+    delete user.password
+    return user
+  }
+}
