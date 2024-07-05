462ea5fa9210e947d15b4f1f307c9dc356565112
Renato Marques <megadeth.destroys.metallica@gmail.com>
2024-07-05 15:19:49 -0300

    preload traversible terrain calculation
    All the values of  and respective  for Terrain Type and Movement Type are preloaded in Gameplay Service's constructor in order to minimize queries in the isTraversible function

--- src/gameplay/gameplay.service.ts
+++ src/gameplay/gameplay.service.ts
@@ -4,10 +4,26 @@ import { MapInfo, MapService } from 'src/map/map.service'
 
 @Injectable()
 export class GameplayService {
+  private terrainTypeIds: Map<string, number>
+  private movementTypeIds: Map<string, number>
+
   constructor(
     private prisma: PrismaService,
     private readonly mapService: MapService,
-  ) {}
+  ) {
+    this.terrainTypeIds = new Map<string, number>()
+    prisma.sqlite.terrainType
+      .findMany({
+        select: { id: true, code: true },
+      })
+      .then((arr) => arr.map((value) => this.terrainTypeIds.set(value.code, value.id)))
+    this.movementTypeIds = new Map<string, number>()
+    prisma.sqlite.movementType
+      .findMany({
+        select: { id: true, code: true },
+      })
+      .then((arr) => arr.map((value) => this.movementTypeIds.set(value.code, value.id)))
+  }
 
   private getTerrainFromMap(pos: [number, number], mapInfo: MapInfo) {
     const [i, j] = pos
@@ -16,18 +32,8 @@ export class GameplayService {
   }
 
   private async isTraversible(terrainTypeCode: string, movementTypeCode: string) {
-    const terrainTypeId = await this.prisma.sqlite.terrainType
-      .findUnique({
-        where: { code: terrainTypeCode },
-        select: { id: true },
-      })
-      .then((value) => value.id)
-    const movementTypeId = await this.prisma.sqlite.movementType
-      .findUnique({
-        where: { code: movementTypeCode },
-        select: { id: true },
-      })
-      .then((value) => value.id)
+    const terrainTypeId = this.terrainTypeIds.get(terrainTypeCode)
+    const movementTypeId = this.movementTypeIds.get(movementTypeCode)
     return this.prisma.sqlite.traversible
       .findUnique({
         where: { movementTypeId_terrainTypeId: { terrainTypeId, movementTypeId } },
@@ -36,7 +42,7 @@ export class GameplayService {
       .then((value) => value.traversible)
   }
 
-  private async isPathValid(
+  private async isValidPath(
     matchId: number,
     userId: number,
     path: [number, number][],
@@ -44,13 +50,11 @@ export class GameplayService {
     movementTypeCode: string,
   ) {
     const info = await this.mapService.getMapInfo(mapId)
-
     if (
       !path.every((pos) => this.isTraversible(this.getTerrainFromMap(pos, info), movementTypeCode))
     ) {
       return false
     }
-
     const opponentId = await this.prisma.postgres.match
       .findUnique({
         where: { id: matchId },
@@ -63,24 +67,19 @@ export class GameplayService {
         select: { position: true },
       })
       .then((arr) => arr.map((value) => value.position))
-
-    const pathSet = new Set(path.sort().map((pair) => String(pair)))
-
+    const pathSet = new Set(path.map((pair) => String(pair)))
     if (enemyUnitPositions.some((enemyPosition) => pathSet.has(enemyPosition))) {
       return false
     }
-
     const friendlyUnitPositions = await this.prisma.postgres.unit
       .findMany({
         where: { userId, matchId },
         select: { position: true },
       })
       .then((arr) => arr.map((value) => value.position))
-
     if (friendlyUnitPositions.some((pos) => String(path[path.length - 1]) === pos)) {
       return false
     }
-
     return true
   }
 }
